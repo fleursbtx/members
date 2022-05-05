644127a900fe884ef5d928084029957ebed2083b
btxfleurs <101543322+btxfleurs@users.noreply.github.com>
2022-05-05 03:45:45 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+document.getElementById('namjoon').addEventListener('click',function() {
+    document.querySelector('#nam').style.display = 'flex';
+});
+document.querySelector('.fechar').addEventListener('click',function(){
+    document.querySelector('#nam').style.display = 'none';
+});
+
+// JIN
+document.getElementById('seokjin').addEventListener('click',function() {
+    document.querySelector('#jin').style.display = 'flex';
+});
+document.querySelector('.fechar-jin').addEventListener('click',function(){
+    document.querySelector('#jin').style.display = 'none';
+});
+
+//YOON
+document.getElementById('yoongi').addEventListener('click',function() {
+    document.querySelector('#yoon').style.display = 'flex';
+});
+document.querySelector('.fechar-yoon').addEventListener('click',function(){
+    document.querySelector('#yoon').style.display = 'none';
+});
+
+// HOBI
+document.getElementById('hoseok').addEventListener('click',function() {
+    document.querySelector('#hobi').style.display = 'flex';
+});
+document.querySelector('.fechar-hobi').addEventListener('click',function(){
+    document.querySelector('#hobi').style.display = 'none';
+});
+
+// JIMIN
+document.getElementById('jimin').addEventListener('click',function() {
+    document.querySelector('#jm').style.display = 'flex';
+});
+document.querySelector('.fechar-jimin').addEventListener('click',function(){
+    document.querySelector('#jm').style.display = 'none';
+});
+
+// TAE
+document.getElementById('taehyung').addEventListener('click',function() {
+    document.querySelector('#tae').style.display = 'flex';
+});
+document.querySelector('.fechar-tae').addEventListener('click',function(){
+    document.querySelector('#tae').style.display = 'none';
+});
+
+// JK
+document.getElementById('jungkook').addEventListener('click',function() {
+    document.querySelector('#jk').style.display = 'flex';
+});
+document.querySelector('.fechar-jk').addEventListener('click',function(){
+    document.querySelector('#jk').style.display = 'none';
+});
